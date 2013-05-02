7b96682401203fa8a4272c688aa221b7cc79ae24
Thomas Maximini <thomas.maximini@medx.net>
2013-05-02 11:36:37 +0200

    chapter 2

---
+++ app/js/config.js
@@ -0,0 +1,7 @@
+define([], function() {
+  var config = {};
+  config.apiKey = 'AIzaSyDcl5KGMeF-SazFk89oMg_3NynoZBaY1ac';
+  config.scopes = 'https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/userinfo.profile';
+  config.clientId = '800345348024.apps.googleusercontent.com';
+  return config;
+});
